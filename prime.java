bebdbbf5462455594dd3157e07ade9b61fc573d1
01fe22bca059 <157386788+01fe22bca059@users.noreply.github.com>
2024-01-24 21:04:38 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -4,7 +4,7 @@ public class prime
    public static void main (String[]args)
    {
      Scanner s=new Scanner(System.in);
-     System.out.println("enter two numbers:");
+     System.out.println("Enter two numbers:");
      int n1=s.nextInt();
      int n2=s.nextInt();
      for (int i = n1; i <= n2; i++)
