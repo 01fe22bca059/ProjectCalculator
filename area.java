9d373ddebe5f39bbd2151bab11dadce42bdf8bba
01FE22BCA059 <01FE22BCA059@kletech.ac.in>
2024-02-03 12:02:51 +0530

    Area of square and triangle

---
+++ area.java
@@ -0,0 +1,23 @@
+/**
+ * The shape class contains methods to calculate the area of a square and a triangle.
+ */
+/**
+ * The shape class contains methods to calculate the area of a square and a triangle.
+ */
+
+class area {
+    int sq_area(int a){
+        return a*a;
+    }
+   float tri_area(int b,int h){
+        float ar=(b*h)/2;
+        return ar;
+    }
+}
+// public class area{
+//       public static void main(String args[]){
+//           shape s=new shape();
+//           System.out.println("area of square is "+s.sq_area(3));
+//           System.out.println("area of triangle is "+s.tri_area(4,5));
+//       }
+//     }
