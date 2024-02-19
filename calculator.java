6e867cd8cfd8ed19277f7c385ca362559f3e1d8c
kanishkkasri g <157386788+01fe22bca059@users.noreply.github.com>
2024-02-19 16:12:36 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -21,12 +21,9 @@ class calculator
 
 public static void main(String args[])
 {
-    Scanner s= new Scanner(System.in);
-    System.out.println("Enter the numbers ");
-
-    calculator c=new calculator();
-float n1=s.nextFloat();
-float n2=s.nextFloat();
+calculator c=new calculator();
+float n1=12.6;
+float n2=7.9;
 System.out.println("Addition:" + c.addition(n1,n2));
 System.out.println("Subtraction:" + c.subtraction(n1,n2));
 System.out.println("Multiplication:" + c.multiplication(n1,n2));
